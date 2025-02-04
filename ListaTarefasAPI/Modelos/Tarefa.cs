a0128ebff384f411346b5af20becbb874c23cf61
Bianca <bianca.furlanetto440@gmail.com>
2025-02-04 15:29:51 -0300

    Adicionando pacotes NuGet

---
+++ ListaTarefasAPI/Modelos/Tarefa.cs
@@ -0,0 +1,11 @@
+﻿namespace ListaTarefasAPI.Modelos
+{
+    public class Tarefa
+    {
+        public int Tarefa { get; set; }
+        public string Descricao { get; set; }
+        public bool concluida { get; set; }
+        public int Tipotarefaid { get; set; }
+        public TipoTarefa TipoTarefa { get; set; }
+    }
+}
---
+++ ListaTarefasAPI/Modelos/TipoTarefa.cs
@@ -0,0 +1,9 @@
+﻿namespace ListaTarefasAPI.Modelos
+{
+    public class TipoTarefa
+    {
+        public int TipoTarefaid { get; set; }
+        public string tipo { get; set; }
+
+    }
+}
