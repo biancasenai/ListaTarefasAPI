5281824c6f4ce8c8ac0b4f2b44c7915485cd6fb0
Bianca <bianca.furlanetto440@gmail.com>
2025-02-05 09:25:01 -0300

    Alteração do get

--- ListaTarefasAPI/Modelos/TipoTarefa.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ListaTarefasAPI.Modelos
-{
-    public class TipoTarefa
-    {
-        public int TipoTarefaid { get; set; }
-        public string tipo { get; set; }
-
-    }
-}
---
+++ ListaTarefasAPI/Models/TipoTarefa.cs
@@ -0,0 +1,9 @@
+﻿namespace ListaTarefasAPI.Models
+{
+    public class TipoTarefa
+    {
+        public int TipoTarefaId { get; set; }
+        public string Tipo { get; set; }
+
+    }
+}
